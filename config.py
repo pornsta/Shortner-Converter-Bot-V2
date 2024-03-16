85ca61b9ac38f3fb67e1ca2621b9aa07afa3d59d
Botyyyy <107973574+pornsta@users.noreply.github.com>
2024-03-16 19:38:35 +0545

    Update config.py

--- config.py
+++ config.py
@@ -9,17 +9,17 @@ load_dotenv()
 # Mandatory variables for the bot to start
 API_ID = int(os.getenv("API_ID", "21159773"))
 API_HASH = os.environ.get("API_HASH", "49ae08543a07335e195756eba2f56e11")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6203515476:AAEKbmZ5IM1Lm9XexXXy9EEhtEAMtEFEo0M")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6548971110:AAGW5_X3noXCCZQWuXm-FtfDvPfS-tTKe7c")
 ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5886772061")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Aaroha")
-DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Aaroha9:Aaroha999@cluster0.jqqzc5t.mongodb.net/?retryWrites=true&w=majority") 
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://aaroha:aaroha@cluster0.f4fgsvs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") 
 OWNER_ID =  int(os.environ.get("OWNER_ID", "5886772061")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(5886772061)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001581944173")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "TGshortener") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002131092358")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "ez4short_official") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
